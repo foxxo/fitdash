33779e392e4938a0d7783313705a8251c32a519e
Tod Schlegelmilch <git@foxxo.net>
2025-03-25 16:33:36 -0400

    fix

--- script.js
+++ script.js
@@ -372,6 +372,8 @@ function displayHeartRateChart(labels, data) {
                 segment: {
                     borderColor: ctx => {
                         const hr = ctx.p1.parsed.y;
+                        const time = new Date(point.parsed.x);
+                        const dateStr = time.toISOString().split('T')[0];
                         const restingHR = window.fitdashOverlayData?.restingHRByDate?.[dateStr] || 66;
                         return getHRGradientColor(hr, restingHR);
                     }

f4acc78cabf658255d27647cdfec2f88d52d4257
Tod Schlegelmilch <git@foxxo.net>
2025-03-25 20:09:07 -0400

    localdatestring method

--- script.js
+++ script.js
@@ -195,6 +195,13 @@ function addDataToChart(chart, newData, date) {
     chart.update();
 }
 
+function getLocalDateString(date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+}
+
 const summaryBubblePlugin = {
     id: 'summaryBubblePlugin',
     beforeDatasetsDraw(chart) {
